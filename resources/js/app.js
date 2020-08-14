3cda1a80c3480d707fe94ba5df2402f497729046
Tigran <tsaturyan.tigran001@gmail.com>
2020-08-14 13:02:19 +0400

    install vue-material

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,12 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(VueMaterial)
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -16,7 +16,6 @@
 
 <script>
     export default {
-        name: "ExampleComponent",
         mounted() {
             console.log('Component mounted.')
         }
